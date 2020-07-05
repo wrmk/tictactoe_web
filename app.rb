f615001f63de1f2237d9b6471a3cd45edf28d0d9
wrmk <elmakan@gmail.com>
2020-07-05 21:31:24 +0300

    add check full playground and random PC choice

--- app.rb
+++ app.rb
@@ -3,14 +3,36 @@ require 'sinatra/reloader'
 
 
 def change_space index
-	if index !=""
+		# изменение выюанной пользователем ячейки
 		@@r[index[1].to_i-1][index[0].to_i-1] = "+"
-	end	
+
+		# проверка на заполненность игрового поля
+		m = 0
+		0.upto(2) do |i|
+			if @@r[i].include?("X")
+				m = m +1
+			end
+		end
+
+		# изменение ячейки компьютером
+		if m >0
+			x = rand(0..2)
+			y = rand(0..2)
+			while @@r[x][y] == "0" or @@r[x][y] == "+"
+				x = rand(0..2)
+				y = rand(0..2)
+			end
+			@@r[x][y] = "0"	
+		end
+
 end
 
+def reset
+	@@r = [['X','X','X'],['X','X','X'],['X','X','X']]
+end
 
 configure do
-	@@r = [['X','X','X'],['X','X','X'],['X','X','X']]
+	reset
 end
 
 get '/' do
@@ -20,7 +42,16 @@ end
 
 
 post '/' do
-@index = params[:button].split
-change_space @index
+
+@index = params[:choose].split
+if @index != ""
+	change_space @index
+end
+
+erb :layout
+end
+
+post '/reset' do
+reset
 erb :layout
 end
