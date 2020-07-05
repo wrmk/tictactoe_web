58b243108fad3c3bd352dd68a21c7dee7f73a00e
wrmk <elmakan@gmail.com>
2020-07-05 19:11:26 +0300

    save selected by user

--- app.rb
+++ app.rb
@@ -1,18 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-before do
-@r = [['X','X','X'],['X','X','X'],['X','X','X']]	
+
+def change_space index
+	if index !=""
+		@@r[index[1].to_i-1][index[0].to_i-1] = "+"
+	end	
 end
 
 
+configure do
+	@@r = [['X','X','X'],['X','X','X'],['X','X','X']]
+end
+
 get '/' do
-  erb :layout
+erb :layout
 end
 
 
 
 post '/' do
-@index = params[:button]
-  erb "#{@index}"
+@index = params[:button].split
+change_space @index
+erb :layout
 end
